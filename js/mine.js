4ce5ccd3545a43744dff0eecc1d65d9d30485414
Nico Van Goethem <n_van_goethem@hotmail.com>
2016-09-09 07:09:32 +0200

    first checkin

---
+++ js/mine.js
@@ -0,0 +1,25 @@
+
+
+function contact(email,content)
+{
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "http://localhost:36382/api/contact",
+      "method": "POST",
+      "headers": {
+        "content-type": "application/json",
+        "cache-control": "no-cache",
+        "postman-token": "59e19f50-8cad-29fc-ed99-223ee0f079f8"
+      },
+      "processData": false,
+      "data": "{\"Email\":\""+ email + "\", \"Content\":\""+content+"\"}"
+    }
+
+    $.ajax(settings).done(function (response) 
+    {
+    console.log(response);
+    }
+    );
+}
+
